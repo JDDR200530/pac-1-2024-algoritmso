da7420efb9f5e8a2c496f035c928f9d48fa06e88
JDDR200530 <david1970josue@gmail.com>
2024-01-30 20:03:40 -0600

    Introduccion js finalizada

---
+++ js/app.js
@@ -0,0 +1,4 @@
+//alert("Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!")
+
+const nombre = prompt('Cual es su nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JS.`;
